04a80d4a356dd6d156acaf3d13fe538c647d1254
KasunjGithub <167292427+KasunjGithub@users.noreply.github.com>
2024-07-20 10:31:13 +0530

    Add files via upload

---
+++ create.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create user</title>
+</head>
+<body>
+    <div class="bg-dark d-flex justify-content-center align-items-center w-100 vh-100">
+    <div class="bg-white w-25 rounded p-3">
+        <form method="POST">
+            <div class="mb-2">
+                <input type="text" name="name" placeholder="enter name"/>
+</div>
+<div class="mb-2">
+    <input type="text" name="email" placeholder="enter email"/>
+</div>
+<div class="mb-2">
+    <input type="text" name="address" placeholder="enter address"/>
+</div>
+<button class="btn btn-success" type="submit">Create</button>
+</form>
+</body>
+</html>
